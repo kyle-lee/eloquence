1bc3c40d5004eca53c6127dc0d56823715e196bd
Kyle Lee <randall.kyle.lee@gmail.com>
2012-11-30 15:01:15 -0800

    Cleanup, ngrams, etc

--- bayes.py
+++ bayes.py
@@ -1,46 +1,85 @@
 import re
+from collections import defaultdict
+import cPickle
+import codecs
 
 
-#TODO: replace this function with a more intelligent n-gram tokenizer
-def get_words(doc):
+def strip_garbage(text):
+    """ Removes any substrings matching any regex contained in the regex
+        file. Used for cleaning the input data prior to training."""
+
+    try:
+        regexes = codecs.open('regex', encoding='utf-8')
+        for regex in regexes.readlines():
+            strip = re.compile(regex.rstrip())
+            text = strip.sub('', text)
+    except IOError:
+        print "Regex file not found."
+
+    return text
+
+
+def get_unigrams(doc):
     splitter = re.compile('\\W*')
-    words = [s.lower() for s in splitter.split(doc)
-            if len(s) > 2 and len(s) < 20]
+    unigrams = [s.lower() for s in splitter.split(doc)]
+    return unigrams
 
-    return dict([(word, 1) for word in words])
+
+def get_ngrams(doc):
+    """ Returns a dictionary containing unigram, bigram,
+        and trigram counts for your wealth and amusement."""
+
+    unigrams = get_unigrams(doc)
+
+    ngrams = []
+
+    bigrams, trigrams = [], []
+    for i in xrange(len(unigrams)):
+        if (i+1) % 2 == 0 and i > 0:
+            ngrams.append(unigrams[i-1] + unigrams[i])
+        if (i+1) % 3 == 0 and i > 0:
+            ngrams.append(unigrams[i-2] + unigrams[i-1] + unigrams[i])
+
+    ngrams.extend(unigrams)
+    ngram_counts = defaultdict(int)
+
+    for gram in ngrams:
+        ngram_counts[gram] += 1
+
+    return dict(ngram_counts)
 
 
 class classifier(object):
 
-    def __init__(self, getfeatures):
-        self.fc = {}
+    def __init__(self, get_features):
+        self.fc_pairs = {}
         self.cc = {}
-        self.getfeatures = getfeatures
+        self.get_features = get_features
 
     # Increase the count of a feature/category pair
-    def incf(self, f, cat):
-        self.fc.setdefault(f, {})
-        self.fc[f].setdefault(cat, 0)
-        self.fc[f][cat] += 1
+    def inc_fc_pair(self, f, cat):
+        self.fc_pairs.setdefault(f, {})
+        self.fc_pairs[f].setdefault(cat, 0)
+        self.fc_pairs[f][cat] += 1
 
     # Increaes the count of a category
-    def incc(self, cat):
+    def inc_cat_count(self, cat):
         self.cc.setdefault(cat, 0)
         self.cc[cat] += 1
 
     # The number of times a feature has appeared in a category
-    def fcount(self, f, cat):
-        if f in self.fc and cat in self.fc[f]:
-            return float(self.fc[f][cat])
+    def f_count(self, f, cat):
+        if f in self.fc_pairs and cat in self.fc_pairs[f]:
+            return float(self.fc_pairs[f][cat])
         return 0.0
 
     # The number of items in a category
-    def catcount(self, cat):
+    def cat_count(self, cat):
         if cat in self.cc:
             return float(self.cc[cat])
 
     # The total number of items:
-    def totalcount(self):
+    def total_count(self):
         return sum(self.cc.values())
 
     # The list of all categories:
@@ -48,34 +87,34 @@ class classifier(object):
         return self.cc.keys()
 
     def train(self, item, cat):
-        features = self.getfeatures(item)
+        features = self.get_features(item)
 
         # Increment the count for every feature with this category
         for f in features:
-            self.incf(f, cat)
+            self.inc_fc_pair(f, cat)
 
         # Increment the count for this category
-        self.incc(cat)
+        self.inc_cat_count(cat)
 
-    def fprob(self, f, cat):
-        if self.catcount(cat) == 0:
+    def f_prob(self, f, cat):
+        if self.cat_count(cat) == 0:
             return
         # The total number of times this feature appeared in this category
         # divided by the total number of items in this category
-        return self.fcount(f, cat) / self.catcount(cat)
+        return self.f_count(f, cat) / self.cat_count(cat)
 
-    def weightedprob(self, f, cat, prf, weight=1.0, ap=0.5):
+    def weighted_prob(self, f, cat, prf, weight=1.0, ap=0.5):
         # Calculate the current probability
         basicprob = prf(f, cat)
 
         # Count the number of times this feature has appeared in all cats
-        totals = sum([self.fcount(f, c) for c in self.categories()])
+        totals = sum([self.f_count(f, c) for c in self.categories()])
 
         # Calculate the weighted average
         bp = ((weight * ap) + (totals * basicprob)) / (weight + totals)
         return bp
 
-    def sampletrain(self):
+    def sample_train(self):
         self.train('Nobody owns the water.', 1)
         self.train('the quick rabbit jumps fences', 1)
         self.train('buy pharmaceuticals now', 0)
@@ -83,24 +122,24 @@ class classifier(object):
         self.train('the quick brown fox jumps', 1)
 
 
-class naivebayes(classifier):
-    def __init__(self, getfeatures):
-        classifier.__init__(self, getfeatures)
+class naive_bayes(classifier):
+    def __init__(self, get_features):
+        classifier.__init__(self, get_features)
         self.thresholds = {}
 
-    def docprob(self, item, cat):
-        features = self.getfeatures(item)
+    def doc_prob(self, item, cat):
+        features = self.get_features(item)
 
         # Multiply the probabilities of all the features together
         p = 1
         for f in features:
-            p *= self.weightedprob(f, cat, self.fprob)
+            p *= self.weighted_prob(f, cat, self.f_prob)
         return p
 
-    def settreshold(self, cat, t):
+    def set_threshold(self, cat, t):
         self.thresholds[cat] = t
 
-    def getthreshold(self, cat):
+    def get_threshold(self, cat):
         if cat not in self.thresholds:
             return 1.0
         else:
@@ -110,9 +149,9 @@ class naivebayes(classifier):
     # Pr(cat|doc) = Pr(doc|cat) * Pr(cat)/Pr(doc)
     # We can ignore Pr(doc) for our purposes
     def prob(self, item, cat):
-        catprob = self.catcount(cat) / self.totalcount()
-        docprob = self.docprob(item, cat)
-        return docprob * catprob
+        cat_prob = self.cat_count(cat) / self.total_count()
+        doc_prob = self.doc_prob(item, cat)
+        return doc_prob * cat_prob
 
     def classify(self, item, default=None):
         probs = {}
@@ -128,7 +167,23 @@ class naivebayes(classifier):
         for cat in probs:
             if cat == best:
                 continue
-            if probs[cat] * self.getthreshold(best) > probs[best]:
+            if probs[cat] * self.get_threshold(best) > probs[best]:
                 return default
 
         return best
+
+    def save(self):
+        try:
+            cPickle.dump(self.fc_pairs, open('fc_pairs.pickle', 'w'))
+            cPickle.dump(self.c, open('cc.pickle', 'w'))
+            cPickle.dump(self.thresholds, open('thresholds.pickle', 'w'))
+        except IOError:
+            print "Could not save files."
+
+    def load(self):
+        try:
+            self.fc_pairs = cPickle.load(open('fc_pairs.pickle'))
+            self.cc = cPickle.load(open('cc.pickle'))
+            self.thresholds = cPickle.load(open('thresholds.pickle'))
+        except IOError:
+            print "Could not load files."
